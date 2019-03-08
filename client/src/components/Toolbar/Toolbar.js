c07dc76bf537a6bceef6ed5fa0d5b1c454f2d5c8
Adibör <44465736+0Adiber@users.noreply.github.com>
2019-03-08 18:56:09 +0100

    6 | changed Toolbar from function to class Component
    function Toolbar(){}
    => class Toolbar extends Component{}

--- client/src/components/Toolbar/Toolbar.js
+++ client/src/components/Toolbar/Toolbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 //Own pages
@@ -8,29 +8,31 @@ import Programming from '../Programming/Programming';
 //CSS
 import './Toolbar.css';
 
-function Toolbar() {
-    return(
-        <Router>
-        <div>
-            <div className="nav">
-                <div className="home">
-                    <Link to="/">Adiber</Link>
+class Toolbar extends Component {
+    render() {
+        return(
+            <Router>
+            <div>
+                <div className="nav">
+                    <div className="home">
+                        <Link to="/">Adiber</Link>
+                    </div>
+                    <ul>
+                        <li>
+                            <Link to="/programming">Programming</Link>
+                        </li>
+                        <li>
+                            <Link to="/webfun">Web-Fun</Link>
+                        </li>
+                    </ul>
                 </div>
-                <ul>
-                    <li>
-                        <Link to="/programming">Programming</Link>
-                    </li>
-                    <li>
-                        <Link to="/webfun">Web-Fun</Link>
-                    </li>
-                </ul>
+                <Route exact path="/" component={Home} />
+                <Route path="/programming" component={Programming} />
+                <Route path="/webfun" component={Home} />
             </div>
-            <Route exact path="/" component={Home} />
-            <Route path="/programming" component={Programming} />
-            <Route path="/webfun" component={Home} />
-        </div>
-        </Router>
-    );
+            </Router>
+        );
+    }
 }
 
 export default Toolbar;
