eb2a0b43be005520c6c8dd58a318ea650bbf9495
0Adiber <0Adiber@users.noreply.github.com>
2019-04-22 10:13:46 +0200

    15 | github
    fetching works, now use githup api !

--- client/src/components/Programming/Programming.js
+++ client/src/components/Programming/Programming.js
@@ -8,6 +8,14 @@ import './Programming.css';
 import bgs from './img/HeaderImg';
 
 class Programming extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          items: []
+        };
+      }
     componentDidMount() {
         $(".nav a").css('color', 'white');
 
@@ -15,7 +23,45 @@ class Programming extends Component {
 
         $("#header").css('background-image', 'url('+bgs[path]+')');
         $(window).scrollTop(0);
+        
+        fetch("http://localhost:33333/github", { method: "GET" })
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result.items
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+
     }
+
+    github() {
+        const {error, isLoaded, items} = this.state;
+        if(error) {
+            return <div>Error: { error.message }</div>
+        } else if(!isLoaded) {
+            return <div>Loading ...</div>
+        } else {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.name}>
+                            {item.name} {item.price}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
+
     render() {
         return(
             <div>
@@ -25,7 +71,7 @@ class Programming extends Component {
                     <div className="inner-content black">
                         <div className="content-wrap-in">
                             <h2>Github repositories</h2>
-                            <div></div>      
+                            <div>{ this.github() }</div>  
                         </div>
                     </div>
                     <div className="inner-content white">
--- server/app.js
+++ server/app.js
@@ -6,13 +6,24 @@ app.listen('33333', function(){
   console.log("listening");
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req, res) {
   res.send("test")
 });
 
 //GITHUB 
 app.get('/github', function(req, res) {
-  res.json({test: 'moin'});
+  res.json({
+    "items": [
+      { "id": 1, "name": "Apples",  "price": "$2" },
+      { "id": 2, "name": "Peaches", "price": "$5" }
+    ] 
+  }).end();
 });
 
 module.exports = app;
