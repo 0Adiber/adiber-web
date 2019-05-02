8f3983b6309a6d07e10ce97d09b3cf57c40be702
Adibör <44465736+0Adiber@users.noreply.github.com>
2019-05-02 19:13:35 +0200

    19 | Contact form
    mysql connection working

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
+var colors = require('colors');
+
+var mysql = require('mysql');
 
 const fetch = require("node-fetch");
 const path = require("path");
 
 app.listen('33333', function(){
-  console.log("Backend listening on Port: 33333");
-  
+  console.log("Backend listening on Port: 33333".black.bgWhite);
+  //mysql connection
+  conToMysql();
   //first github get req
   fetchGithub();
 });
@@ -53,7 +57,7 @@ function fetchGithub() {
           size: t.size 
         });
       });
-      console.log("Updated Github Repos Successfully!");
+      console.log("\nUpdated Github Repos Successfully!".random);
     },
     (error) => {
       gitItems.items = {error: error.message}
@@ -77,3 +81,74 @@ module.exports = app;
 /*
 * Github OVER
 */
+
+/*
+* CONTACT Start
+*/
+
+app.post('/contact', function(req, res) {
+  console.log(req.body);
+  res.send("noise");
+});
+
+/*
+* CONTACT OVER
+*/
+
+
+//MYSQL
+function conToMysql() {
+
+  console.log("----------------------");
+  console.log("MYSQL:".yellow.bold);
+
+  const data = require('./_configs/mysql.json');
+
+  global.connection = mysql.createConnection({
+    host: data.host,
+    user: data.username,
+    passoword: data.passoword
+  });
+
+  //connect
+  connection.connect(function(err) {
+    if(err){
+      console.log("Can't connect to Database".red.bold)
+      throw err;
+    }
+    console.log("Connected with MYSQL!".green.underline);
+  });
+  //create database
+  connection.query("CREATE DATABASE IF NOT EXISTS adiber_web", function(err, result) {
+    if(err){
+      console.log("Can't create Database".red.bold)
+      throw err;
+    }
+    if(result.warningCount > 0){
+      console.log("Webpage Database already exists!".black.bgWhite)
+    } else {
+      console.log("Webpage Database created!".green.underline)
+    }
+  });
+  //select database
+  connection.query("USE adiber_web", function(err, result){
+    if(err){
+      console.log("Can't select Database".red.bold)
+      throw err;
+    }
+    console.log("Selected Database 'adiber_web'".black.bgWhite);
+  });
+  //create table
+  connection.query("CREATE TABLE IF NOT EXISTS contacts (uid INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255), message VARCHAR(1023))", function(err, result) {
+    if(err){
+      console.log("Can't create Table".red.bold)
+      throw err;
+    }
+    if(result.warningCount > 0){
+      console.log("Contacts Table exists!".black.bgWhite)
+    } else {
+      console.log("Contacts Table created!".green.underline)
+    }
+    console.log("----------------------")
+  });
+}
